6f36d7d85b518dfb7a5cc4316ad3f6478b8b671e
davidsretzlaff <davids.retzlaff@gmail.com>
2025-02-12 07:39:52 -0300

    creating first main.tf with a VM

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
+#1
 terraform {
+  required_version = "~>1.0" // aceitar versões acima de 1
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean" // specifying the source, which is DigitalOcean
@@ -9,4 +11,19 @@ terraform {
     }
   }
 }
-//test
+
+# 2
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = "dop_v1_32b63616c1c47813bb73d0b69512ee13352741bfdb5544c54fafba2fb08879c7" // token created on provider
+}
+
+# 3
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "vm_aula" { // segundo label é nome que voce quer dar ao do resource
+  image   = "ubuntu-22-04-x64"
+  name    = "vm-aula2"
+  region  = "nyc1"
+  size    = "s-1vcpu-512mb-10gb" // link slugs.do-api.dev
+}
+
