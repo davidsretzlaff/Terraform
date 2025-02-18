58753e77f70895864dd6c593594d7e32e7b69f6f
davidsretzlaff <davids.retzlaff@gmail.com>
2025-02-18 07:52:24 -0300

    learning about variables types in terraform

--- local-cloud-variables/main.tf
+++ local-cloud-variables/main.tf
@@ -1,18 +1,3 @@
-resource "local_file" "foo_list_string" {
-  content  = "The value i will use in my machine is ${var.content_list_string[0]}"
-  filename = "./arquivo.txt"
-}
-
-
-resource "local_file" "foo_list_number" {
-  content  = "The value i will use in my machine is Machine ${var.content_list_number[0]}"
-  filename = "./arquivo.txt"
-}
-
-resource "local_file" "foo_map" {
-  content  = "The value i will use in my machine is Machine ${var.content_map["small"]}"
-  filename = "./arquivo.txt"
-}
 
 variable "content_string" {
   default     = "file content"
@@ -32,28 +17,84 @@ variable "content_boolean" {
   description = "boolean variable"
 }
 
+resource "local_file" "foo_list_string" {
+  content  = "The value i will use in my machine is ${var.content_list_string[0]}"
+  filename = "./arquivo.txt"
+}
 
 variable "content_list_string" {
   default     = ["Machine01", "Machine02", "Machine03"]
   type        = list(string)
-  description = "boolean variable"
+  description = "list string variable"
 }
 
 
+resource "local_file" "foo_list_number" {
+  content  = "The value i will use in my machine is Machine ${var.content_list_number[0]}"
+  filename = "./arquivo.txt"
+}
 
 variable "content_list_number" {
   default     = [1, 2, 3]
   type        = list(number)
-  description = "boolean variable"
+  description = "list number variable"
 }
 
 
+resource "local_file" "foo_map" {
+  content  = "The value i will use in my machine is Machine ${var.content_map["small"]}"
+  filename = "./arquivo.txt"
+}
+
 variable "content_map" {
-  default     = {
+  default = {
     "small" = "s-1vcpu-2gb"
-    "mid" = "s-4vcpu-4gb"
+    "mid"   = "s-4vcpu-4gb"
     "large" = "s-8vcpu-8gb"
   }
   type        = map(string)
-  description = "boolean variable"
+  description = "map variable"
+}
+
+
+
+resource "local_file" "foo_set" {
+  content  = "The value i will use in my machine is ${join(", ", var.content_set)}"
+  filename = "./arquivo.txt"
+}
+
+variable "content_set" {
+  default     = ["s-1vcpu-2gb", "s-4vcpu-4gb", "s-8vcpu-8gb"]
+  type        = set(string)
+  description = "set variable"
+}
+
+resource "local_file" "foo_object" {
+  content  = "The value i will use in my machine is ${var.content_object.option_machine}"
+  filename = "./arquivo.txt"
+}
+
+variable "content_object" {
+  default = {
+    region           = "sp",
+    option_machine   = "machine2",
+    should_do_backup = true
+  }
+  type = object({ 
+      region = string,
+      option_machine = string,
+      should_do_backup = bool
+    })
+  description = "object variable"
+}
+
+resource "local_file" "foo_tuple" {
+  content  = "The value i will use in my machine is ${var.content_tuple[0]}"
+  filename = "./arquivo.txt"
+}
+
+variable "content_tuple" {
+  default = ["sp", "machine2", true]
+  type = tuple([string, string, bool])
+  description = "tuple variable"
 }
--- local-cloud-variables/terraform.tfvars
+++ local-cloud-variables/terraform.tfvars
@@ -1,7 +1,7 @@
 content_string = "new value"
 
 content_map = {
-    "small" = "s-1vcpu-2gb"
-    "mid" = "s-4vcpu-4gb"
-    "large" = "s-8vcpu-8gb"
-  }
+  "small" = "s-1vcpu-2gb"
+  "mid"   = "s-4vcpu-4gb"
+  "large" = "s-8vcpu-8gb"
+}
