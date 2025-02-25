7988bf6813fb7eee997e740225689643dff2db8b
davidsretzlaff <davids.retzlaff@gmail.com>
2025-02-25 07:54:40 -0300

    expression

--- expression/main.tf
+++ expression/main.tf
@@ -1,12 +1,26 @@
-resource "local_file" "expression_aritimetica" {
+resource "local_file" "arithmetic_expression" {
   content = 50 + 10 + var.value
-  filename = "expression_aritimetica.txt"
+  filename = "arithmetic_expression.txt"
+}
+
+resource "local_file" "logical_expression" {
+  content = var.value == 10
+  filename = "logical_expression.txt"
 }
 
 
-resource "local_file" "expression_logica" {
-  content = var.value == 10
-  filename = "expression_logica.txt"
+resource "local_file" "condition_expression" {
+  content = var.value > 10 ? "greater" : "smaller"
+  filename = "condition_expression.txt"
+}
+
+resource "local_file" "join_for" {
+  content = join(", ", [for v in var.join_for_value: upper(v)])
+  filename = "join_for.txt"
+}
+
+variable "join_for_value" {
+  default = ["Docker","Jenkins","Terraform",]
 }
 
 variable "value" {
