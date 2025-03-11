fa40736c930be12b2c1782e8d2d3f44b0026b9b1
davidsretzlaff <davids.retzlaff@gmail.com>
2025-03-11 10:29:37 -0300

    terraform modules cloud

---
+++ terraform-modules-cloud/variables.tf
@@ -0,0 +1,19 @@
+variable "do_token" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "nyc1"
+}
+
+variable "wp_vm_count" {
+  type = number
+  default = 2
+  description = "number of machines for wordpress"
+
+  validation {
+    condition = var.wp_vm_count > 1
+    error_message = "the minimum number of machines required is two"
+  }
+}
