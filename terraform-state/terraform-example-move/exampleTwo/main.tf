27943832787b5b8db9fb2e11c9d94df2907eeb4c
davidsretzlaff <davids.retzlaff@gmail.com>
2025-03-17 22:04:00 -0300

    terraform state rm

--- terraform-state/terraform-example-move/exampleTwo/main.tf
+++ terraform-state/terraform-example-move/exampleTwo/main.tf
@@ -1,3 +1,7 @@
 module module_pet {
   source = "./module/file-pet" 
 }
+
+//terraform init to load the new module
+// terraform state mv local_file.file module.file_pet.local_file.file
+// terraform state mv "random_pet.pets" "module.file_pet.random_pet.pets"
---
+++ terraform-state/terraform-example-state-rm/main.tf
@@ -0,0 +1,16 @@
+resource "local_file" "file" {
+  content = random_pet.pets[count.index].id 
+  filename = "${count.index}-file.txt"  
+  count = var.quantity
+}
+
+resource "random_pet" "pets" {
+    count = var.quantity
+}
+
+variable "quantity" {
+    default = 5
+}
+
+// terraform state rm local_file.file
+// terraform state rm random_pet.pets
