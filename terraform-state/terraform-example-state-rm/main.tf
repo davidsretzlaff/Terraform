55faef134d1aa823a6cda1fc85b0ecf55947d8b8
davidsretzlaff <davids.retzlaff@gmail.com>
2025-03-18 09:36:01 -0300

    terraform state rm

--- terraform-state/terraform-example-state-rm/main.tf
+++ terraform-state/terraform-example-state-rm/main.tf
@@ -1,16 +1,19 @@
 resource "local_file" "file" {
-  content = random_pet.pets[count.index].id 
-  filename = "${count.index}-file.txt"  
-  count = var.quantity
+  content  = random_pet.pets[count.index].id
+  filename = "${count.index}-file.txt"
+  count    = var.quantity
 }
 
 resource "random_pet" "pets" {
-    count = var.quantity
+  count = var.quantity
 }
 
 variable "quantity" {
-    default = 5
+  default = 6
 }
 
 // terraform state rm local_file.file
 // terraform state rm random_pet.pets
+// if you made a mistake and want to cancel, use the command: force the file recreate 
+// terraform state rm 'local_file.file[1]'
+// terraform state list | grep local_file
